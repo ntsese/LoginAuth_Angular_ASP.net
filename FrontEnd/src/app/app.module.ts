1123692d6ad2d391ccf4ce62d0ff058f09a78ad0
samuel sesing <211012394@edu.vut.ac.za>
2023-11-14 20:07:45 +0200

    UI code for login component and signup component pages.

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/signUp/signUp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
