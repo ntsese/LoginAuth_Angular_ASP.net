ec8f10c343d97831ea8811fac6fe7dba042b0e6d
samuel sesing <211012394@edu.vut.ac.za>
2023-11-26 18:46:59 +0200

    add route configuration for login and signup components

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/signUp/signUp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "Login", component: LoginComponent},
+  {path: "signup", component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
