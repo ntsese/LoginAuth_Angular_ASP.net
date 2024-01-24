d260e21aeafc59318e97326f2ca5face3ec22cfe
samuel <211012394@edu.vut.ac.za>
2024-01-24 13:25:32 +0200

    add css, and validation on signup component

--- FrontEnd/src/app/components/signUp/signUp.component.ts
+++ FrontEnd/src/app/components/signUp/signUp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signUp',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signUpForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router ) { }
 
   ngOnInit() {
+    this.signUpForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
 }
