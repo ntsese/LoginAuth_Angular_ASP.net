17e800e69a15a2e2827955dfe91d9da845e54a2a
samuel <211012394@edu.vut.ac.za>
2024-02-05 16:16:06 +0200

    ASP.net webAPI

---
+++ BackEnd/AngularAuthAPI/AngularAuthAPI/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularAuthAPI.Models
+{
+    public class Users
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
+    }
+}
