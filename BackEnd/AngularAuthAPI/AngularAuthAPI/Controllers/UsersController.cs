fe170d42b4f9c3b4ba1bc6d98eedd155c9716a7a
samuel <211012394@edu.vut.ac.za>
2024-02-18 15:48:24 +0200

    **Subject: Adding JWT token generation to UsersController**
    This commit implements a method for JSON Web Tokens (JWT) in the UsersController. This enables secure authentication by providing users with tokens after successful login.
    
    **Key features**
    *Generates JWT tokens based on user claims
    *Uses a cryptograhic signing algorith for token integrity
    *Configureable token expiration time
    
    **Benefits**
    *Enhance security by implementing token-base authentication
    *Improves user experience by eliminating the need for session management

--- BackEnd/AngularAuthAPI/AngularAuthAPI/Controllers/UsersController.cs
+++ BackEnd/AngularAuthAPI/AngularAuthAPI/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using AngularAuthAPI.Context;
+using AngularAuthAPI.Helpers;
 using AngularAuthAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -6,7 +7,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AngularAuthAPI.Controllers
 {
@@ -34,7 +39,40 @@ namespace AngularAuthAPI.Controllers
             if (user == null)
                 return NotFound(new { Message = "User not found" });
 
-          
+          if (PasswordHasher.verifyPassword(userObj.Password, user.Password))
+            {
+                return BadRequest(new { message = "password is incorrect" });
+            }
+
+          userObj.Token = GenerateJwt(user)
+        }
+
+
+        //Generate jwt tokens
+        private string GenerateJwt(Users users)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            //Create key
+            var key = Encoding.ASCII.GetBytes("SECRET USED TO VERIFY JWT TOKENS..");
+
+            //Create payLoader
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, users.Role),
+                new Claim(ClaimTypes.Name, users.FirstName, users.FirstName)
+            });
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+
+            return jwtTokenHandler.WriteToken(token);
         }
 
 
