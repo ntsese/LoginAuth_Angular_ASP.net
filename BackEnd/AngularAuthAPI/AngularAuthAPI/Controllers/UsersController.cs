8b2f07336e3ade018bda04edc666c9ab3feaa05b
samuel <211012394@edu.vut.ac.za>
2024-02-19 18:30:20 +0200

    **Subject: Implemented user registration with enhaced validation**
    This commit adds a 'RegistrUser' method to facilitate user registration. The method incorporates robust validation checks to ensure data integrity and security:
    
    - **User existence:** Employs 'CheckUserExists' to prevent duplicate registrations.
    - **Email validity:** Levarages 'CheckEmailExists' to verify unique email addresses.
    - **Password strength:** Enforces password cpmplexity using regular expressions defined in 'checkPasswordStrength'.
    
    **Benefits**
    -Improved data quality and security through comprehesive validation.
    - Enhanced user experience with clear feedback on registration errors.

--- BackEnd/AngularAuthAPI/AngularAuthAPI/Controllers/UsersController.cs
+++ BackEnd/AngularAuthAPI/AngularAuthAPI/Controllers/UsersController.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using System.Text;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
+using System.Text.RegularExpressions;
 
 namespace AngularAuthAPI.Controllers
 {
@@ -44,8 +45,70 @@ namespace AngularAuthAPI.Controllers
                 return BadRequest(new { message = "password is incorrect" });
             }
 
-          userObj.Token = GenerateJwt(user)
+            userObj.Token = GenerateJwt(user);
+
+            return Ok(new
+            {
+                Token = user.Token,
+                Message = "Login Success"
+            });
         }
+
+        //method to check if username exist
+        private Task<bool> CheckUserNameExistAsync(string username) =>
+            _authContext.User.AnyAsync(x => x.Username == username);
+
+
+        //method to check if email exist
+        private Task<bool> CheckUserEmailExistAsync(string email) =>
+            _authContext.User.AnyAsync(x => x.Username == email);
+
+        
+        //method to check password strength
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (password.Length < 9)
+                sb.Append("Minimum password should be length be 8" + Environment.NewLine);
+
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]")))  
+                sb.Append("Password should be alphanumeric" + Environment.NewLine);
+
+            if (!Regex.IsMatch(password, "[<,>,@,!,#,$,%,^,&,*,(,),_,+,\\[,\\],{,},?,:,;,|,',\\,.,/,~,`,-,=]"))
+                sb.Append("Password should contain special characters" + Environment.NewLine);
+
+            return sb.ToString();
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser([FromBody] Users userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+
+            //Check the username
+            if (await CheckUserNameExistAsync(userObj.Username))
+                return BadRequest(new { Message = "Username Already Exists" });
+
+            //check the password strength
+            var passwd = CheckPasswordStrength(userObj.Password);
+            if (string.IsNullOrEmpty(passwd))
+                return BadRequest(new { message = passwd.ToString() });
+
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "User";
+            userObj.Token = "";
+            await _authContext.User.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "User registered"
+            });
+        }
+ 
 
 
         //Generate jwt tokens
