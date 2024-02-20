dcd4ededc5279da546fe6a04a0e8865702142bfd
samuel <211012394@edu.vut.ac.za>
2024-02-20 16:43:59 +0200

    Implemented authenticate service for user login and registration

---
+++ FrontEnd/src/app/services/authenticate.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class AuthenticateService {
+private baseUrl: string = ""
+
+constructor(private http: HttpClient) { }
+
+loginUser (loginObj: any){
+  return this.http.post<any>(this.baseUrl + "authenticate", loginObj);
+}
+
+
+registerUser (userProfile: any){
+  return this.http.post<any>(this.baseUrl + "register", userProfile);
+}
+}
